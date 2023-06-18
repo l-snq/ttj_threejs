57a002d5695efceaa7c0106a19501689d19e205b
tastetest <tastytothesupreme@gmail.com>
2023-06-17 22:17:43 -0600

    i don't know whats going on at all. It just doesn't like me :(

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -1,10 +1,37 @@
+'use client';
+import { createRoot } from 'react-dom/client'
 import * as THREE from 'three'
-import React from 'react';
+import React, {useState, useRef, useEffect } from 'react';
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 
-export default function page() {
-  return (
-    <div>
-      <p>helloooooo</p>
-    </div>
+function Cube(props: ThreeElements['mesh']) {
+    const [active, setActive] = useState(false);
+    const [hovered, setHover] = useState(false);
+
+    useFrame((state, delta) => (mesh.current.rotation.x += delta))
+
+    const mesh = useRef<THREE.Mesh>(null!);
+
+    return (
+      <mesh
+        {...props}
+        ref={mesh}
+        scale={active ? 2.0 : 1}
+              onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}>
+      
+         <boxGeometry args={[1, 1, 1]} />
+         <meshStandardMaterial color={'hotpink'} />
+      </mesh>
+    )
+};
+
+createRoot(document.getElementById('root')!).render(
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Cube position={[-1.2, 0, 0]} />
+      <Cube position={[1.2, 0, 0]} />
+    </Canvas>,
   )
-}
