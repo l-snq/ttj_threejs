9a9ad30a8da91fb6fdf83d65d37c33f89ad5f918
tastetest <tastytothesupreme@gmail.com>
2023-06-17 20:58:28 -0600

    added in react fibre threejs

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -2,22 +2,6 @@ import * as THREE from 'three'
 import React from 'react';
 
 export default function page() {
-
-  const scene = new THREE.Scene();
-
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  const renderer = new THREE.WebGLRenderer();
-
-  renderer.setSize( window.innerHeight, window.innerWidth );
-  document.body.appendChild( renderer.domElement );
-
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
-
   return (
     <div>
       <p>helloooooo</p>
