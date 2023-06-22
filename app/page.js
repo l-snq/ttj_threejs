0b32290e67093baa47a5a2b3ba348f43e679a568
tastetest <tastytothesupreme@gmail.com>
2023-06-22 17:52:25 -0600

    added in rotation

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
-import React, { Suspense, useEffect, useReducer } from 'react'
-import {Canvas, useLoader } from '@react-three/fiber'
+import React, { Suspense, useEffect, useRef, useReducer } from 'react'
+import { Canvas, useLoader, useFrame } from '@react-three/fiber'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
@@ -10,15 +10,20 @@ function Load() {
     const interval = setInterval(toggle, 1000)
     return () => clearInterval(interval)
   }, [])
+
+  const ref = useRef()
+    useFrame((state, delta) => (ref.current.rotation.y += delta))
   const { scene } = useLoader(GLTFLoader, flag ? './character.glb' : './character.glb') 
-  return <primitive object={scene} />
+  return <primitive ref={ref} object={scene}>
+    </primitive>
 }
 
 export default function Home() {
 
   return (
     <main>
-      <Canvas>
+      <Canvas style={{height: '100vh'}}>
+        <pointLight position={[10, 10, 10]} />
         <Suspense fallback={null}>
           <Load />
         </Suspense>
