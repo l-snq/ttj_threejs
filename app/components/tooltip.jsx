aaa24a2f4bada25f013084cae61ec10f8433ba8d
ttj <tastytothesupreme@gmail.coom>
2023-06-27 21:01:49 -0600

    fixed styling for tooltip(sorta)

--- app/components/tooltip.jsx
+++ app/components/tooltip.jsx
@@ -5,9 +5,11 @@ import { Html } from '@react-three/drei'
 export const CustomTooltip = () => {
   return (
     <Html center>
-      <Tooltip title="Type 'cd about' to go to my about page.">
-	  <span> View commands </span>
-      </Tooltip>
-    </Html>
+      <div style={{display: 'flex', marginTop: '10em'}}>
+	<Tooltip title="Type 'cd about' to go to my about page.">
+	    <span> View commands </span>
+	</Tooltip>
+     </div>
+       </Html>
   )
 }
