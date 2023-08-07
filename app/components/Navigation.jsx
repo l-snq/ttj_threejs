e4811da41b099d41f20d913f538225c79daef512
tastetest <tastytothesupreme@gmail.com>
2023-08-06 19:15:06 -0600

    added navbar

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -6,18 +6,50 @@ import {
   PieChartOutlined,
 } from '@ant-design/icons';
 import Link from 'next/link';
-import {Button, menu} from 'antd'
+import {Button, Menu} from 'antd'
 
 export const Navigation = () => {
+    function getItem(label, key, icon, children, type) {
+      return {
+	key,
+	icon, 
+	children,
+	label,
+	type,
+      }
+    }
+
+    const items = [
+      getItem("Options", "option1", null, [
+	getItem(<li><Link href="/">home</Link></li>, "0"),
+	getItem(<li><Link href="/about">about</Link></li>, "1"),
+	getItem(<li><Link href="/contact">contact</Link></li>, "2"),
+	getItem(<li><Link href="/mywork">mywork</Link></li>, "3"),
+      ])
+    ]
+    const onClick = (e) => {
+      console.log("click", e);
+    }
     return (
       <>
 	<div style={{display: 'flex', justifycontent: 'center', flexdirection: 'row'}}>
-	  <ul style={{liststyletype: 'none'}}>
+	  <ul style={{liststyletype: 'none', flexDirection: 'row'}}>
 	    <li><Link href="/">home</Link></li>
-	    <li> <Link href="/about">about</Link></li>
-	    <li> <Link href="/contact"> contact me </Link></li>
-	    <li> <Link href="/mywork"> my work </Link> </li>
+	    <li><Link href="/about">about</Link></li>
+	    <li><Link href="/contact"> contact me </Link></li>
+	    <li><Link href="/mywork"> my work </Link> </li>
 	  </ul>
+
+	  <Menu
+	    onClick={onClick}
+	    style={{width: 256,}}
+	    defaultSelectedKeys={
+	      ['1']
+	    }
+	    defaultOpenKeys={['sub1']}
+	    mode="inline"
+	    items={items}
+	  />
 	</div>
       </>
     )
