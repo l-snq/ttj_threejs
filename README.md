e9d2d80f2a8220b9c945b4922e1584103fbd1f48
ttjdesktop <tastytothesupreme@gmail.com>
2024-08-04 22:07:51 -0600

    updated readme again

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 The goal of this demo is to show off some of my skills regarding threejs.
 
+TODO!:
+
+Add controls to move the character around
+
--- app/LoadModel.jsx
+++ app/LoadModel.jsx
@@ -1,22 +1,23 @@
 'use client'
-import React, { Suspense, useEffect, useRef, useReducer } from 'react'
-import { Canvas, useLoader, useFrame } from '@react-three/fiber'
+import React, { useEffect, useRef, useReducer } from 'react'
+import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
+import { useLoader, useFrame } from '@react-three/fiber'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 export default function LoadModel () {
 	const [flag, toggle] = useReducer((state) => !state, true)
-	// const [isLoading, setIsLoading] = useState();
 	
-	  useEffect(() => {
+        useEffect(() => {
 	    const interval = setInterval(toggle, 1000)
 	    return () => clearInterval(interval)
-	  }, [])
+	}, [])
 
-	  const ref = useRef()
+	const ref = useRef()
 
-	  useFrame((state, delta) => (ref.current.rotation.y += delta))
+	//useFrame((state, delta) => (ref.current.rotation.y += delta))
 
-	  const { scene } = useLoader(GLTFLoader, flag ? './character.glb' : './character.glb') 
+	const { scene } = useLoader(GLTFLoader, 
+		  flag ? './character.glb' : './character.glb') 
 	return <primitive ref={ref} object={scene}>
-    </primitive>
+	</primitive>
 }
